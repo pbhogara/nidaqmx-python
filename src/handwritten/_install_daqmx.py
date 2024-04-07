3ff362a7ad76d0b8f0e71a5b1ed5f21625eee543
Praneeth Bhogaraju <praneeth.bhogaraju@ni.com>
2024-04-07 11:46:14 +0530

    Get started by implementing the flow for linux installation

--- src/handwritten/_install_daqmx.py
+++ src/handwritten/_install_daqmx.py
@@ -4,23 +4,62 @@ import importlib.resources as pkg_resources
 import json
 import logging
 import os
+import shutil
 import pathlib
 import subprocess
 import sys
 import tempfile
 import traceback
 import urllib.request
+import zipfile
 from typing import Generator, List, Optional, Tuple
 
 import click
 
 if sys.platform.startswith("win"):
     import winreg
+elif sys.platform == "linux":
+    import distro
 
 _logger = logging.getLogger(__name__)
 
 METADATA_FILE = "_installer_metadata.json"
 
+def _get_ubuntu_installation_commands(_directory_to_extract_to: str) -> List[List[str]]:
+
+    UBUNTU_COMMANDS =  [
+                            ['sudo', 'apt', 'update'],
+                            ['sudo', 'apt', 'install', f'{_directory_to_extract_to}/NILinux2024Q1DeviceDrivers/ni-ubuntu2004-drivers-2024Q1.deb'],
+                            ['sudo', 'apt', 'update'],
+                            ['sudo', 'apt', 'install', 'ni-daqmx'],
+                            ['sudo', 'apt', 'install', 'ni-hwcfg-utility'],
+                            ['sudo', 'dkms', 'autoinstall']
+                    ]
+    return UBUNTU_COMMANDS
+
+def _get_opensuse_installation_commands(_directory_to_extract_to: str) -> List[List[str]]:
+    OPENSUSE_COMMANDS = [
+                            ['sudo', 'zypper', 'update'],
+                            ['sudo', 'zypper', 'install', 'insserv'],
+                            ['sudo', 'zypper', '--no-gpg-checks', 'install', f'{_directory_to_extract_to}/NILinux2024Q1DeviceDrivers/ni-opensuse154-drivers-2024Q1.rpm'],
+                            ['sudo', 'zypper', 'refresh'],
+                            ['sudo', 'zypper', 'install', 'ni-daqmx'],
+                            ['sudo', 'zypper', 'install', 'ni-hwcfg-utility'],
+                            ['sudo', 'dkms', 'autoinstall']
+                        ]
+    return OPENSUSE_COMMANDS
+
+def _get_redhat_installation_commands(_directory_to_extract_to: str) -> List[List[str]]:
+                
+    REDHAT_COMMANDS =   [
+                            ['sudo', 'yum', 'update'],
+                            ['sudo', 'zypper', 'install', 'chkconfig'],
+                            ['sudo', 'yum', 'install', f'{_directory_to_extract_to}/NILinux2024Q1DeviceDrivers/ni-rfhel8-drivers-2024Q1.rpm'],
+                            ['sudo', 'yum', 'install', 'ni-daqmx'],
+                            ['sudo', 'yum', 'install', 'ni-hwcfg-utility'],
+                            ['sudo', 'dkms', 'autoinstall']
+                        ]
+    return REDHAT_COMMANDS
 
 def _parse_version(version: str) -> Tuple[int, ...]:
     """
@@ -116,8 +155,35 @@ def _multi_access_temp_file(
                     f"Failed to delete temporary file '{temp_file.name}'.\nDetails: {e}"
                 ) from e
 
+@contextlib.contextmanager
+def _multi_access_temp_folder(*, delete: bool = True) -> Generator[str, None, None]:
+    """
+    Context manager for creating a temporary file.
 
-def _load_data(json_data: str) -> Tuple[Optional[str], Optional[str]]:
+    """
+    try:
+        temp_folder = tempfile.TemporaryDirectory()
+        _logger.debug("Created temp folder: %s", temp_folder.name)
+    except Exception as e:
+        _logger.info("Failed to create temporary folder.", exc_info=True)
+        raise click.ClickException(
+            f"Failed to create temporary folder '{temp_folder.name}'.\nDetails: {e}"
+        ) from e
+
+    try:
+        yield temp_folder.name
+    finally:
+        if delete:
+            try:
+                _logger.debug("Deleting temp file: %s", temp_folder.name)
+                shutil.rmtree(temp_folder.name)
+            except ValueError as e:
+                _logger.info("Failed to delete temporary file.", exc_info=True)
+                raise click.ClickException(f"Failed to delete temporary folder '{temp_folder.name}'.\nDetails: {e}") from e
+
+
+
+def _load_data(json_data: str, platform: str) -> Tuple[Optional[str], Optional[str], Optional[List[str]]]:
     """
     Load data from JSON string and extract Windows metadata.
 
@@ -136,7 +202,12 @@ def _load_data(json_data: str) -> Tuple[Optional[str], Optional[str]]:
 
     """
     try:
-        metadata = json.loads(json_data).get("Windows", [])
+        if (platform == "Windows"):
+            metadata = json.loads(json_data).get("Windows", [])
+        elif (platform == "Linux"):
+            metadata = json.loads(json_data).get("Linux", [])
+        else:
+            raise click.ClickException(f"Unsupported os '{platform}'")
     except json.JSONDecodeError as e:
         _logger.info("Failed to parse the json data.", exc_info=True)
         raise click.ClickException(f"Failed to parse the driver metadata.\nDetails: {e}") from e
@@ -144,13 +215,14 @@ def _load_data(json_data: str) -> Tuple[Optional[str], Optional[str]]:
     for metadata_entry in metadata:
         location: Optional[str] = metadata_entry.get("Location")
         version: Optional[str] = metadata_entry.get("Version")
+        supported_os: Optional[List[str]] = metadata_entry.get("supportedOS")
         _logger.debug("From metadata file found location %s and version %s.", location, version)
         if location and version:
-            return location, version
+            return location, version, supported_os
     raise click.ClickException(f"Unable to fetch driver details")
 
 
-def _get_driver_details() -> Tuple[Optional[str], Optional[str]]:
+def _get_driver_details(platform: str) -> Tuple[Optional[str], Optional[str], Optional[List[str]]]:
     """
     Parse the JSON data and retrieve the download link and version information.
 
@@ -158,8 +230,8 @@ def _get_driver_details() -> Tuple[Optional[str], Optional[str]]:
     try:
         with pkg_resources.open_text(__package__, METADATA_FILE) as json_file:
             _logger.debug("Opening the metadata file %s.", METADATA_FILE)
-            location, version = _load_data(json_file.read())
-        return location, version
+            location, version, supported_os = _load_data(json_file.read(), platform)
+        return location, version, supported_os
 
     except click.ClickException:
         raise
@@ -238,7 +310,7 @@ def _install_daqmx_windows_driver() -> None:
 
     """
     installed_version = _get_daqmx_installed_version()
-    download_url, latest_version = _get_driver_details()
+    download_url, latest_version, supported_os = _get_driver_details("Windows")
     if not download_url:
         raise click.ClickException(f"Failed to fetch the download url.")
     else:
@@ -247,6 +319,61 @@ def _install_daqmx_windows_driver() -> None:
         else:
             _install_daqmx_driver(download_url)
 
+def _install_daqmx_linux_driver() -> None:
+    """
+    Install the NI-DAQmx driver on Linux.
+
+    """
+    dist_info = distro.linux_distribution()
+    dist_name = dist_info[0].lower()
+    dist_version = dist_info[1]
+    _dist_name_and_version = dist_name + " " + dist_version
+
+    download_url, latest_version, supported_os = _get_driver_details("Linux")
+
+    # Check if the platform is one of the supported ones
+    if _dist_name_and_version in supported_os:
+         _logger.info(f"The platform is supported: {dist_name} {dist_version}", exc_info=True)
+    else:
+        raise click.ClickException(f"The platform {_dist_name_and_version} is not supported.")
+    
+    try:
+        with _multi_access_temp_file(suffix=".zip") as temp_file:
+            _logger.info("Downloading Driver to %s", temp_file)
+            urllib.request.urlretrieve(download_url, temp_file)
+
+            with _multi_access_temp_folder() as temp_folder:
+                _directory_to_extract_to = temp_folder
+
+            _logger.info("Unzipping the downloaded file to %s", _directory_to_extract_to)
+
+            with zipfile.ZipFile(temp_file, 'r') as zip_ref:
+                zip_ref.extractall(_directory_to_extract_to)
+
+            _logger.info("Installing NI-DAQmx")
+            if dist_name == "ubuntu":
+                for command in _get_ubuntu_installation_commands(_directory_to_extract_to):
+                    subprocess.run(command, check=True)
+            elif dist_name == "opensuse":
+                for command in OPENSUSE_COMMANDS:
+                    subprocess.run(command, check=True)
+            elif dist_name == "redhat":
+                for command in REDHAT_COMMANDS:
+                    subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        _logger.info("Failed to installed NI-DAQmx driver.", exc_info=True)
+        raise click.ClickException(
+            f"An error occurred while installing the NI-DAQmx driver. Command returned non-zero exit status '{e.returncode}'."
+        ) from e
+    except urllib.error.URLError as e:
+        _logger.info("Failed to download NI-DAQmx driver.", exc_info=True)
+        raise click.ClickException(f"Failed to download the NI-DAQmx driver.\nDetails: {e}") from e
+    except Exception as e:
+        _logger.info("Failed to install NI-DAQmx driver.", exc_info=True)
+        raise click.ClickException(f"Failed to install the NI-DAQmx driver.\nDetails: {e}") from e
+
+    if not download_url:
+        raise click.ClickException(f"Failed to fetch the download url.")
 
 def installdriver() -> None:
     """
@@ -257,8 +384,11 @@ def installdriver() -> None:
         if sys.platform.startswith("win"):
             _logger.info("Windows platform detected")
             _install_daqmx_windows_driver()
+        elif sys.platform == "linux":
+            _logger.info("Linux platform detected")
+            _install_daqmx_linux_driver()
         else:
-            raise click.ClickException(f"The 'installdriver' command is supported only on Windows.")
+            raise click.ClickException(f"The 'installdriver' command is supported only on Windows and Linux.")
     except click.ClickException:
         raise
     except Exception as e:
